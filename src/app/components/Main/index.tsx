1ee64410aa9fc82ccffb92380bc002fc0ecd1a23
Kelvis Delapena <kelvis.silvadelapena@AB-inbev.com>
2025-01-07 17:09:19 -0300

    chore: initial jest tests

---
+++ src/app/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <header>
+      <h1>REACT</h1>
+    </header>
+  )
+}
+
+export default Main
---
+++ src/app/components/Main/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />)
+
+    expect(screen.getByText(/react/i)).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+})
